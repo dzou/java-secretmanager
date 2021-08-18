bfd173a4bd31db5d0f2188101e9e269f840516dd
dzou <dzou@users.noreply.github.com>
2021-08-18 16:28:20 -0400

    run tests

--- .kokoro/graalvm-testing.sh
+++ .kokoro/graalvm-testing.sh
@@ -3,15 +3,6 @@
 # Fail on any error.
 set -e
 
-# Display commands being run.
-# WARNING: please only enable 'set -x' if necessary for debugging, and be very
-#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
-#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
-#  the credentials being printed in build logs.
-#  Additionally, recursive invocation with credentials as command-line
-#  parameters, will print the full command, with credentials, in the build logs.
-set -x
-
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
@@ -42,5 +33,6 @@ gu install native-image
 # Test Java
 java -version
 
-
+# Run Unit and Integration Tests
+mvn test -Pnative -Penable-integration-tests
 
