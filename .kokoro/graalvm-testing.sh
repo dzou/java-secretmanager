840cbe05ae550cb45681a347326907a260e35a86
dzou <dzou@users.noreply.github.com>
2021-08-18 16:19:18 -0400

    rm newline

--- .kokoro/graalvm-testing.sh
+++ .kokoro/graalvm-testing.sh
@@ -27,7 +27,6 @@ echo ${JOB_TYPE}
 # Install GraalVM
 graalvmDir=${KOKORO_ARTIFACTS_DIR}/graalvm
 mkdir ${graalvmDir}
-
 retry_with_backoff 3 10 \
   curl --fail --show-error --silent --location \
   https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.2.0/graalvm-ce-java11-linux-amd64-21.2.0.tar.gz \
